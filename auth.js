316f1f3cae6d10466af446725b8617ff64db4c7f
pham hoang <phamhoang.dev@gmail.com>
2021-05-25 16:59:15 +0700

    deploy calendar backend to heroku

---
+++ auth.js
@@ -0,0 +1,67 @@
+const jwt = require('jsonwebtoken');
+const db = require("./db");
+
+const getToken = (req) => {
+  let authorization = null;
+  let token = null;
+  if (req.query && req.query.token) {
+    return req.query.token;
+  }
+  if (req.authorization) {
+    authorization = req.authorization;
+  } else if (req.headers) {
+    authorization = req.headers.authorization;
+  } else if (req.socket) {
+    if (req.socket.handshake.query && req.socket.handshake.query.token) {
+      return req.socket.handshake.query.token;
+    }
+    authorization = req.socket.handshake.headers.authorization;
+  }
+  if (authorization) {
+    const tokens = authorization.split('Bearer ');
+    if (Array.isArray(tokens) || tokens.length === 2) {
+      [, token] = tokens;
+    }
+  }
+  return token;
+}
+
+const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, 'SECRET_TOKEN :)', (err, data) => {
+      if (err) {
+        return {
+          message: 'Forbidden',
+          status: 403,
+        };
+      } else {
+        const user = db.get('users').find({ id: data.user }).value();
+        return user;
+      }
+    });
+  } catch (error) {
+    throw new Error()
+  }
+}
+
+exports.auth = () => {
+  return async (req, res, next) => {
+    try {
+      const token = getToken(req);
+      if (!token) {
+        return res.status(401).json({
+          message: 'authentication_is_failure',
+          status: 401,
+        });
+      }
+      const user = await verifyToken(token);
+      if (user.status && user.status === 403) {
+        return res.status(403).json(user);
+      }
+      req.user = user;
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+  };
+}
---
+++ index.js
@@ -0,0 +1,103 @@
+// checkout my spaghetti code
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+// remember the good old days 
+const db = require("./db");
+
+const requireAuth = require('./auth');
+
+app.use(cors());
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+
+app.get('/', (req, res, next) => {
+  res.json('hello ;3');
+})
+// SECRET_TOKEN :)))
+const generateAccessToken = (user) => jwt.sign({ user }, 'SECRET_TOKEN :)', { expiresIn: '1d' });
+
+app.post('/api/auth/login', (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = db.get('users').find({ email }).value();
+    if (!user) {
+      return res.status(401).json({
+        message: 'incorrect_email_or_password',
+        statusCode: 401,
+      });
+    }
+
+    const match = bcrypt.compareSync(password, user.password);
+    if (!match) {
+      return res.status(401).json({
+        message: 'wrong_password',
+        statusCode: 401,
+      });
+    }
+    const accessToken = generateAccessToken(user.id);
+    delete user.password;
+    return res.status(200).json({
+      token: {
+        tokenType: 'Bearer',
+        accessToken,
+        expiresIn: new Date(new Date().getTime() + 86400000),
+      },
+      user,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.get('/api/events', requireAuth.auth(), (req, res, next) => {
+  // tại sao lại 200 ? vì không có token hay token lỗi thì 401 hay 403
+  // ở requireAuth rồi :> 
+  const user = req.user;
+  res.status(200).json({
+    events: user.events
+  });
+});
+
+app.post('/api/events', requireAuth.auth(), (req, res, next) => {
+  const user = req.user;
+  const { title = '', date = '', description = '' } = req.body;
+
+  // never trust FE even it's my code (")> 
+  // i dun wanna use validate libray just for this, so this is a fastest way 
+  if (!title.trim() && !date.trim() && !description.trim()) {
+    return res.status(400).json({
+      message: 'some_field_is_missing_or_wrong_type',
+      statusCode: 400,
+    });
+  }
+
+  if (new Date(date.trim())) {
+    return res.status(400).json({
+      message: 'date_field_is_not_a_date',
+      statusCode: 400,
+    });
+  }
+
+  db.get('users').find({ id: user.id }).assign({
+    events: [...user.events, {
+      title, date, description
+    }]
+  }).write();
+
+  return res.status(200).json({
+    events: db.get('users').find({ id: user.id }).value().events,
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+});
+// end of spaghetti code
